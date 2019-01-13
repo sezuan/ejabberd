33f9aff011d5033b9725c25b61c5a8a4c166e2b4
Matthias Rieber <matthias@zu-con.org>
2019-01-13 20:50:30 +0000

    Use own record for leveldb

--- include/mod_mam.hrl
+++ include/mod_mam.hrl
@@ -22,8 +22,18 @@
 	{us = {<<"">>, <<"">>} :: {binary(), binary()},
 	 timestamp = p1_time_compat:timestamp() :: erlang:timestamp()}).
 
--record(archive_msg,
+-record(archive_msg_lvl,
 	{us = #ust{}                          :: #ust{},
+	 id = <<>>                            :: binary() | '_',
+	 timestamp = p1_time_compat:timestamp() :: erlang:timestamp() | '_' | '$1',
+	 peer = {<<"">>, <<"">>, <<"">>}      :: ljid() | '_' | '$3' | undefined,
+	 bare_peer = {<<"">>, <<"">>, <<"">>} :: ljid() | '_' | '$3',
+	 packet = #xmlel{}                    :: xmlel() | message() | '_',
+	 nick = <<"">>                        :: binary(),
+	 type = chat                          :: chat | groupchat}).
+
+-record(archive_msg,
+	{us = {<<"">>, <<"">>}                :: {binary(), binary()} | '$2',
 	 id = <<>>                            :: binary() | '_',
 	 timestamp = p1_time_compat:timestamp() :: erlang:timestamp() | '_' | '$1',
 	 peer = {<<"">>, <<"">>, <<"">>}      :: ljid() | '_' | '$3' | undefined,
