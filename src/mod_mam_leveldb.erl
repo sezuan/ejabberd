c1e1d14f4daf642f96a7a2cf74349f6b1eecfb9e
Matthias Rieber <matthias@zu-con.org>
2019-01-20 13:47:46 +0000

    Remove id. Move timestamp to #ust{}

--- src/mod_mam_leveldb.erl
+++ src/mod_mam_leveldb.erl
@@ -30,6 +30,9 @@
 -export([init/2, remove_user/2, remove_room/3, delete_old_messages/3,
 	 extended_fields/0, store/8, write_prefs/4, get_prefs/2, select/6, remove_from_archive/3]).
 
+%% DEBUG EXPORTS
+-export([timestamp_to_binary/1, binary_to_timestamp/1]).
+
 -include_lib("stdlib/include/ms_transform.hrl").
 -include("xmpp.hrl").
 -include("logger.hrl").
@@ -51,9 +54,9 @@ init(_Host, _Opts) ->
     mnesia_eleveldb:register(),
     try
 	{atomic, _} = ejabberd_mnesia:create(
-			?MODULE, archive_msg_lvl,
+			?MODULE, archive_msg_set,
 			[{leveldb_copies, [node()]},
-			 {attributes, record_info(fields, archive_msg_lvl)}]),
+			 {attributes, record_info(fields, archive_msg_set)}]),
 	{atomic, _} = ejabberd_mnesia:create(
 			?MODULE, archive_prefs,
 			[{disc_only_copies, [node()]},
@@ -64,7 +67,7 @@ init(_Host, _Opts) ->
     end.
 
 delete_us({Table, US}) ->
-    Msgs = mnesia:match_object(Table, #archive_msg_lvl{
+    Msgs = mnesia:match_object(Table, #archive_msg_set{
 		us = #ust{us = US, timestamp = '_'}, _ = '_'}, read),
     lists:foreach(fun(X) ->
             mnesia:delete_object(X)
@@ -73,7 +76,7 @@ delete_us({Table, US}) ->
 remove_user(LUser, LServer) ->
     US = {LUser, LServer},
     F = fun () ->
-		delete_us({archive_msg_lvl, US}),
+		delete_us({archive_msg_set, US}),
 		mnesia:delete({archive_prefs, US})
 	end,
     mnesia:transaction(F).
@@ -83,7 +86,7 @@ remove_room(_LServer, LName, LHost) ->
 
 remove_from_archive(LUser, LServer, none) ->
     US = {LUser, LServer},
-    case mnesia:transaction(fun () -> delete_us({archive_msg_lvl, US}) end) of
+    case mnesia:transaction(fun () -> delete_us({archive_msg_set, US}) end) of
 	{atomic, _} -> ok;
 	{aborted, Reason} -> {error, Reason}
     end;
@@ -91,7 +94,7 @@ remove_from_archive(LUser, LServer, WithJid) ->
     US = {LUser, LServer},
     Peer = jid:remove_resource(jid:split(WithJid)),
     F = fun () ->
-	    Msgs = mnesia:match_object(#archive_msg_lvl{us = #ust{us = US, _ = '_'}, bare_peer = Peer, _ = '_'}),
+	    Msgs = mnesia:match_object(#archive_msg_set{us = #ust{us = US, _ = '_'}, bare_peer = Peer, _ = '_'}),
 	    lists:foreach(fun mnesia:delete_object/1, Msgs)
 	end,
     case mnesia:transaction(F) of
@@ -100,22 +103,22 @@ remove_from_archive(LUser, LServer, WithJid) ->
     end.
 
 delete_old_messages(global, TimeStamp, Type) ->
-    delete_old_user_messages(mnesia:dirty_first(archive_msg_lvl), TimeStamp, Type).
+    delete_old_user_messages(mnesia:dirty_first(archive_msg_set), TimeStamp, Type).
 
 delete_old_user_messages('$end_of_table', _TimeStamp, _Type) ->
     ok;
 delete_old_user_messages(User, TimeStamp, Type) ->
     F = fun() ->
-		case mnesia:read(archive_msg_lvl, User) of
-			[#archive_msg_lvl{timestamp = MsgTS,
-				     type = MsgType}] when MsgTS < TimeStamp andalso
+		case mnesia:read(archive_msg_set, User) of
+			[#archive_msg_set{us = #ust{timestamp = MsgTS},
+					  type = MsgType}] when MsgTS < TimeStamp andalso
 							   (Type =:= all orelse
 							    Type =:= MsgType) ->
-                           mnesia:delete({archive_msg_lvl, User});
+                           mnesia:delete({archive_msg_set, User});
                         _ -> ok
 	        end
 	end,
-    NextRecord = mnesia:dirty_next(archive_msg_lvl, User),
+    NextRecord = mnesia:dirty_next(archive_msg_set, User),
     case mnesia:transaction(F) of
 	{atomic, ok} ->
 	    delete_old_user_messages(NextRecord, TimeStamp, Type);
@@ -128,8 +131,8 @@ extended_fields() ->
     [].
 
 store(Pkt, _, {LUser, LServer}, Type, Peer, Nick, _Dir, TS) ->
-    case {mnesia:table_info(archive_msg_lvl, disc_only_copies),
-	  mnesia:table_info(archive_msg_lvl, memory)} of
+    case {mnesia:table_info(archive_msg_set, disc_only_copies),
+	  mnesia:table_info(archive_msg_set, memory)} of
 	{[_|_], TableSize} when TableSize > ?TABLE_SIZE_LIMIT ->
 	    ?ERROR_MSG("MAM archives too large, won't store message for ~s@~s",
 		       [LUser, LServer]),
@@ -138,9 +141,8 @@ store(Pkt, _, {LUser, LServer}, Type, Peer, Nick, _Dir, TS) ->
 	    LPeer = {PUser, PServer, _} = jid:tolower(Peer),
 	    F = fun() ->
 			mnesia:write(
-			  #archive_msg_lvl{us = #ust{us = {LUser, LServer}},
-				           id = integer_to_binary(TS),
-				           timestamp = misc:usec_to_now(TS),
+			  #archive_msg_set{us = #ust{us = {LUser, LServer},
+						     timestamp = misc:usec_to_now(TS)},
 				           peer = LPeer,
 				           bare_peer = {PUser, PServer, <<>>},
 				           type = Type,
@@ -169,8 +171,10 @@ get_prefs(LUser, LServer) ->
     end.
 
 change_record_type(Record, NewType) ->
-    [_|R] = tuple_to_list(Record),
-    list_to_tuple([NewType|R]).
+    [_, US|R] = tuple_to_list(Record),
+    #ust{timestamp = Timestamp} = US,
+    Id = timestamp_to_binary(Timestamp),
+    list_to_tuple([NewType,US,Id,Timestamp] ++ R).
 
 select(_LServer, JidRequestor,
        #jid{luser = LUser, lserver = LServer} = JidArchive,
@@ -182,8 +186,13 @@ select(_LServer, JidRequestor,
 	       true -> undefined
 	    end,
     MS = make_matchspec(LUser, LServer, Start, End, LWith),
-    Msgs = mnesia:dirty_select(archive_msg_lvl, MS),
-    SortedMsgs = lists:keysort(#archive_msg_lvl.timestamp, Msgs),
+    Msgs = mnesia:dirty_select(archive_msg_set, MS),
+    SortedMsgs = lists:sort(
+		   fun(A, B) ->
+			   #archive_msg_set{us = #ust{timestamp = T1}} = A,
+			   #archive_msg_set{us = #ust{timestamp = T2}} = B,
+			   T1 =< T2
+		   end, Msgs),
     {FilteredMsgs, IsComplete} = filter_by_rsm(SortedMsgs, RSM),
     Count = length(Msgs),
     Result = {lists:flatmap(
@@ -193,7 +202,7 @@ select(_LServer, JidRequestor,
 			       Msg, MsgType, JidRequestor, JidArchive) of
 			    {ok, El} ->
 				[{Msg#archive_msg.id,
-				  binary_to_integer(Msg#archive_msg.id),
+				  Msg#archive_msg.id,
 				  El}];
 			    {error, _} ->
 				[]
@@ -210,7 +219,7 @@ make_matchspec(LUser, LServer, Start, undefined, With) ->
     make_matchspec(LUser, LServer, Start, [], With);
 make_matchspec(LUser, LServer, Start, End, {_, _, <<>>} = With) ->
     ets:fun2ms(
-      fun(#archive_msg_lvl{us = #ust{us = US, timestamp = TS},
+      fun(#archive_msg_set{us = #ust{us = US, timestamp = TS},
 		           bare_peer = BPeer} = Msg)
 	    when Start =< TS, End >= TS,
 		 US == {LUser, LServer},
@@ -219,7 +228,7 @@ make_matchspec(LUser, LServer, Start, End, {_, _, <<>>} = With) ->
       end);
 make_matchspec(LUser, LServer, Start, End, {_, _, _} = With) ->
     ets:fun2ms(
-      fun(#archive_msg_lvl{us = #ust{us = US, timestamp = TS},
+      fun(#archive_msg_set{us = #ust{us = US, timestamp = TS},
 		           peer = Peer} = Msg)
 	    when Start =< TS, End >= TS,
 		 US == {LUser, LServer},
@@ -228,7 +237,7 @@ make_matchspec(LUser, LServer, Start, End, {_, _, _} = With) ->
       end);
 make_matchspec(LUser, LServer, Start, End, undefined) ->
     ets:fun2ms(
-      fun(#archive_msg_lvl{us = #ust{us = US, timestamp = TS},
+      fun(#archive_msg_set{us = #ust{us = US, timestamp = TS},
 		           peer = Peer} = Msg)
 	    when Start =< TS, End >= TS,
 		 US == {LUser, LServer} ->
@@ -242,16 +251,16 @@ filter_by_rsm(_Msgs, #rsm_set{max = Max}) when Max < 0 ->
 filter_by_rsm(Msgs, #rsm_set{max = Max, before = Before, 'after' = After}) ->
     NewMsgs = if is_binary(After), After /= <<"">> ->
 		      lists:filter(
-			fun(#archive_msg_lvl{id = I}) ->
-				?BIN_GREATER_THAN(I, After)
+			fun(#archive_msg_set{us = #ust{timestamp = I}}) ->
+				I > binary_to_timestamp(After)
 			end, Msgs);
 		 is_binary(Before), Before /= <<"">> ->
 		      lists:foldl(
-			fun(#archive_msg_lvl{id = I} = Msg, Acc)
-				when ?BIN_LESS_THAN(I, Before) ->
-				[Msg|Acc];
-			   (_, Acc) ->
-				Acc
+			fun(#archive_msg_set{us = #ust{timestamp = I}} = Msg, Acc) ->
+			    case I < binary_to_timestamp(Before) of
+				true -> [Msg|Acc];
+				false -> Acc
+			    end
 			end, [], Msgs);
 		 is_binary(Before), Before == <<"">> ->
 		      lists:reverse(Msgs);
@@ -266,3 +275,14 @@ filter_by_max(Msgs, Len) when is_integer(Len), Len >= 0 ->
     {lists:sublist(Msgs, Len), length(Msgs) =< Len};
 filter_by_max(_Msgs, _Junk) ->
     {[], true}.
+
+-spec timestamp_to_binary(erlang:timestamp()) -> binary().
+timestamp_to_binary(Timestamp) ->
+    base64:encode(term_to_binary(Timestamp)).
+
+-spec binary_to_timestamp(binary()) -> erlang:timestamp().
+binary_to_timestamp(Binary) ->
+    case catch binary_to_term(base64:decode(Binary)) of
+	{'EXIT', _} -> <<"">>;
+	Timestamp -> Timestamp
+    end.
